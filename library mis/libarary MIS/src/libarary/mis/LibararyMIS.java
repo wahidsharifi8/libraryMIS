762bd44e2bec27650cef3008c63871d3ea38f422
Abdul Wahid Sharifi <135712175+wahidsharifi8@users.noreply.github.com>
2023-11-06 22:14:16 +0430

    Add files via upload

---
+++ library mis/libarary MIS/src/libarary/mis/LibararyMIS.java
@@ -0,0 +1,12 @@
+package libarary.mis;
+
+public class LibararyMIS {
+
+ 
+    public static void main(String[] args) {
+        loginframe lo = new loginframe();
+        lo.setVisible(true);
+        
+    }
+    
+}
